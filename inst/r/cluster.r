58a50badecd1510e68ab6a1286d607a00dbe0763
Alain D <alain.danet@caramail.fr>
2018-09-03 15:24:46 +0200

    Add bifurcation simulation for high indirect facilitation

--- inst/r/cluster.r
+++ inst/r/cluster.r
@@ -6,7 +6,7 @@ library(tidyverse)
 library(magrittr)
 devtools::load_all()
 
-options(mc.cores = 24)
+options(mc.cores = 20)
 
 ##########################
 #  Complement to bifurc  #
@@ -22,35 +22,11 @@ output <- run_scenarii_gradient(
   gradient = gradient,
   model_spec = "two_facilitation_model",
   param = c(protection_type = list("first_protect"), gamma1 = 0.1),
+  scenarii = init_scenarii(type = "bifurcation", ini_cover = .8, low_cover = .1),
   set_tail = 10
   )
+
+options(mc.cores = 5)
 scenar_avg <- avg_runs(output, cut_row = 10)
 rm(output)
 save(scenar_avg, file = "scenar_birfuc_u_10.RData")
-rm(scenar_avg)
-
-##################
-#  Cooccurences  #
-##################
-
-set.seed(123)
-gradient <- list(
-  u = seq(0, 10, by = .1),
-  del = seq(1, 0, by = -.01),
-  g = c(0.1, .2)
-  )
-output <- run_scenarii_gradient(
-  gradient = gradient,
-  model_spec = "ca_two_facilitation_model",
-  param = c(protection_type = list("first_protect"), gamma1 = 0.1, b = .8),
-  time_seq = c(from = 0, to = 10000, by = .5),
-  set_tail = 300, nrep = 10
-  )
-
-save(output, file = "scenar_ca_cooccurence.Rdata")
-
-options(mc.cores = 5)
-scenar_avg <- avg_runs(output, cut_row = 300)
-rm(output)
-save(scenar_avg, file = "scenar_avg_ca_cooccurence.Rdata")
-rm(scenar_avg)
