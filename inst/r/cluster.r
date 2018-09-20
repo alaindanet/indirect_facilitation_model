35a863e317204badb8c2043d3d72a27469bdac0e
Alain D <alain.danet@caramail.fr>
2018-09-20 17:52:28 +0200

    New sim

--- inst/r/cluster.r
+++ inst/r/cluster.r
@@ -13,23 +13,22 @@ options(mc.cores = 20)
 ##########################
 
 gradient <- list(
-  u = 10,
-  b = seq(1, 0.5, by = -0.005),
-  g = seq(0, .5, 0.005)
+  u = seq(0, 10, by = .1),
+  del = seq(1, 0, by = -.01)
   )
 
 output <- run_scenarii_gradient(
   gradient = gradient,
   model_spec = "two_facilitation_model",
-  param = c(protection_type = list("first_protect"), gamma1 = 0.1),
-  scenarii = init_scenarii(type = "bifurcation", ini_cover = .8, low_cover = .01),
+  param = c(protection_type = list("first_protect"), gamma1 = 0.1, g = .2, b = .9),
+  scenarii = init_scenarii(type = "together", ini_cover = .8),
   set_tail = 10
   )
 
 options(mc.cores = 5)
 scenar_avg <- avg_runs(output, cut_row = 10)
 rm(output)
-save(scenar_avg, file = "scenar_birfuc_u_10.RData")
+save(scenar_avg, file = "clustering_pa_avg.RData")
 
 
 #################
@@ -37,24 +36,18 @@ save(scenar_avg, file = "scenar_birfuc_u_10.RData")
 #################
 
 #Not run (around 120h with 20 cores)
-#set.seed(123)
-#gradient <- list(
-  #u = seq(0, 10, by = .1),
-  #del = seq(1, 0, by = -.01),
-  #g = c(0.1, .2)
-  #)
-#output <- run_scenarii_gradient(
-  #gradient = gradient,
-  #model_spec = "ca_two_facilitation_model",
-  #param = c(protection_type = list("first_protect"), gamma1 = 0.1, b = .8),
-  #time_seq = c(from = 0, to = 10000, by = .5),
-  #set_tail = 300, nrep = 10
-  #)
-#save(output, file = "scenar_ca_cooccurence.Rdata")
+set.seed(123)
+output <- run_scenarii_gradient(
+  gradient = gradient,
+  model_spec = "ca_two_facilitation_model",
+  param = c(protection_type = list("first_protect"), gamma1 = 0.1, b = .9, g = .2),
+  time_seq = c(from = 0, to = 10000, by = .5),
+  set_tail = 300, nrep = 10
+  )
+save(output, file = "scenar_ca_cooccurence.Rdata")
 
-#options(mc.cores = 5)
-#scenar_avg <- avg_runs(output, cut_row = 300)
-#rm(output)
-#save(scenar_avg, file = "scenar_avg_ca_cooccurence.Rdata")
-#rm(scenar_avg)
-
+options(mc.cores = 5)
+scenar_avg <- avg_runs(output, cut_row = 300)
+rm(output)
+save(scenar_avg, file = "clustering_ca_avg.Rdata")
+rm(scenar_avg)
