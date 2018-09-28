e7196c7fbf04a95484669ac7b2174396fd5534d3
Alain D <alain.danet@caramail.fr>
2018-09-28 10:31:44 +0200

    Simulations: effect of direct facilitation on clustering

--- inst/r/cluster.r
+++ inst/r/cluster.r
@@ -6,21 +6,21 @@ library(tidyverse)
 library(magrittr)
 devtools::load_all()
 
-options(mc.cores = 20)
+options(mc.cores = 24)
 
 ##########################
 #  Complement to bifurc  #
 ##########################
 
 gradient <- list(
-  u = seq(0, 10, by = .1),
+  f = seq(0, 1, by = .01),
   del = seq(1, 0, by = -.01)
   )
 
 output <- run_scenarii_gradient(
   gradient = gradient,
   model_spec = "two_facilitation_model",
-  param = c(protection_type = list("first_protect"), gamma1 = 0.1, g = .2, b = .9),
+  param = c(protection_type = list("first_protect"), gamma1 = 0.1, g = .2, b = .9, u = 5),
   scenarii = init_scenarii(type = "together", ini_cover = .8),
   set_tail = 10
   )
@@ -28,7 +28,7 @@ output <- run_scenarii_gradient(
 options(mc.cores = 5)
 scenar_avg <- avg_runs(output, cut_row = 10)
 rm(output)
-save(scenar_avg, file = "clustering_pa_avg.RData")
+save(scenar_avg, file = "direct_facilitation_effect_pa_avg.RData")
 
 
 #################
@@ -36,18 +36,20 @@ save(scenar_avg, file = "clustering_pa_avg.RData")
 #################
 
 #Not run (around 120h with 20 cores)
+options(mc.cores = 24)
 set.seed(123)
 output <- run_scenarii_gradient(
   gradient = gradient,
   model_spec = "ca_two_facilitation_model",
-  param = c(protection_type = list("first_protect"), gamma1 = 0.1, b = .9, g = .2),
+  param = c(protection_type = list("first_protect"), gamma1 = 0.1, b = .9, g =
+.2, u = 5),
   time_seq = c(from = 0, to = 10000, by = .5),
   set_tail = 300, nrep = 10
   )
-save(output, file = "scenar_ca_cooccurence.Rdata")
+save(output, file = "direct_facilitation_effect_ca_cooccurence.RData")
 
 options(mc.cores = 5)
 scenar_avg <- avg_runs(output, cut_row = 300)
 rm(output)
-save(scenar_avg, file = "clustering_ca_avg.Rdata")
+save(scenar_avg, file = "direct_facilitation_effect_ca_avg.RData")
 rm(scenar_avg)
