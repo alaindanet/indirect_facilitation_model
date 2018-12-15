3dde59acc6892747fadebb3ba34a1885760f4500
Alain Danet <alain.danet@umontpellier.fr>
2018-12-15 06:39:52 -0500

    Update cluster script specification

--- inst/r/cluster_all.sh
+++ inst/r/cluster_all.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-nohup ./cluster_stability.r &
+./cluster_stability.r
+
+wait
+
 echo "Stability done!"
 
-nohup ./cluster_cooccurence.r &
+./cluster_cooccurence.r
+
+wait
+
 echo "Cooccurence done!"
 
+# Kill multiprocess generate by nohup:
+# https://unix.stackexchange.com/questions/279642/why-i-couldnt-kill-the-nohup-process
+# ps aux | grep r | grep ./cluster_stability.r # This will show you the process you're looking for
+# kill $(ps aux | grep r | grep ./cluster_stability.r | awk '{print $2}') # This will kill it.
